6b3322661f2a84d34f8fa538a7770ee079603a88
Jason A. Thomas <71987755+AlpacasSayQuack@users.noreply.github.com>
2020-10-26 12:46:41 -0800

    Update and rename source-txt.py to source.py

--- source.py
+++ source.py
@@ -6,7 +6,6 @@ def openwrite(client):
     date_format = "%m-%d-%Y"
     contact_date = today.strftime(date_format)
     file = open(f'{client}.txt', 'a')
-    # file = open(client, 'a')
     file.write(f"\n{contact_date}")
     print(client, contact_date)
     file.close()
